07aceb32d6730719ba362d270c4cabe7ee35ebf2
Loai Dev <loaidev64@gmail.com>
2023-07-08 10:04:35 +0300

    store card

--- routes/api/Card.php
+++ routes/api/Card.php
@@ -2,9 +2,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CardController;
 
-Route::apiResource('/card',CardController::class);
+Route::middleware('auth:sanctum')->apiResource('/card',CardController::class);
 //Route::get('/card',[App\Http\Controllers\CardController::class,'index']);
 Route::delete('/card/{card}', [App\Http\Controllers\CardController::class, 'destroy'])->name('card.destroy');
-?>
-
-
